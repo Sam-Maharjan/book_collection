abfaf18b6cc1e112827f55a351690432171a4bf0
Sam Maharjan <samaharjan1738@gmail.com>
2026-01-31 19:58:58 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Book.destroy_all
+
+Book.create!([
+  { title: "Clean Code", author: "Robert Martin", price: 35 },
+  { title: "Pragmatic Programmer", author: "Andy Hunt", price: 30 },
+  { title: "Eloquent Ruby", author: "Russ Olsen", price: 25 },
+  { title: "Refactoring", author: "Martin Fowler", price: 40 },
+  { title: "Design Patterns", author: "GoF", price: 45 }
+])
