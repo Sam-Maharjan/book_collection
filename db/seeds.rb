910d60ac1f6ab0824f131aad20dcce6ff372f7b0
Sam Maharjan <samaharjan1738@gmail.com>
2026-01-31 20:53:31 -0600

    Fix seeds published_date

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 Book.destroy_all
 
 Book.create!([
-  { title: "Clean Code", author: "Robert Martin", published_on: Date.new(2008, 8, 1) },
-  { title: "The Pragmatic Programmer", author: "Andrew Hunt", published_on: Date.new(1999, 10, 30) },
-  { title: "Design Patterns", author: "Erich Gamma", published_on: Date.new(1994, 10, 21) },
-  { title: "Refactoring", author: "Martin Fowler", published_on: Date.new(1999, 7, 8) },
-  { title: "Eloquent Ruby", author: "Russ Olsen", published_on: Date.new(2011, 2, 11) },
-  { title: "Programming Ruby", author: "Dave Thomas", published_on: Date.new(2000, 9, 15) },
-  { title: "Ruby on Rails Tutorial", author: "Michael Hartl", published_on: Date.new(2022, 1, 1) },
-  { title: "Algorithms", author: "Robert Sedgewick", published_on: Date.new(2011, 3, 19) }
+  { title: "Clean Code", author: "Robert Martin", published_date: Date.new(2008, 8, 1) },
+  { title: "The Pragmatic Programmer", author: "Andrew Hunt", published_date: Date.new(1999, 10, 30) },
+  { title: "Design Patterns", author: "Erich Gamma", published_date: Date.new(1994, 10, 21) },
+  { title: "Refactoring", author: "Martin Fowler", published_date: Date.new(1999, 7, 8) },
+  { title: "Eloquent Ruby", author: "Russ Olsen", published_date: Date.new(2011, 2, 11) },
+  { title: "Programming Ruby", author: "Dave Thomas", published_date: Date.new(2000, 9, 15) },
+  { title: "Ruby on Rails Tutorial", author: "Michael Hartl", published_date: Date.new(2022, 1, 1) },
+  { title: "Algorithms", author: "Robert Sedgewick", published_date: Date.new(2011, 3, 19) }
 ])
