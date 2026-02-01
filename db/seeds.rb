2315d9d701f58faa08230049f23304be47534087
Sam Maharjan <samaharjan1738@gmail.com>
2026-01-31 20:44:18 -0600

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
 Book.destroy_all
 
 Book.create!([
-  { title: "Clean Code", author: "Robert Martin", price: 35 },
-  { title: "Pragmatic Programmer", author: "Andy Hunt", price: 30 },
-  { title: "Eloquent Ruby", author: "Russ Olsen", price: 25 },
-  { title: "Refactoring", author: "Martin Fowler", price: 40 },
-  { title: "Design Patterns", author: "GoF", price: 45 }
+  { title: "Clean Code", author: "Robert Martin", published_on: Date.new(2008, 8, 1) },
+  { title: "The Pragmatic Programmer", author: "Andrew Hunt", published_on: Date.new(1999, 10, 30) },
+  { title: "Design Patterns", author: "Erich Gamma", published_on: Date.new(1994, 10, 21) },
+  { title: "Refactoring", author: "Martin Fowler", published_on: Date.new(1999, 7, 8) },
+  { title: "Eloquent Ruby", author: "Russ Olsen", published_on: Date.new(2011, 2, 11) },
+  { title: "Programming Ruby", author: "Dave Thomas", published_on: Date.new(2000, 9, 15) },
+  { title: "Ruby on Rails Tutorial", author: "Michael Hartl", published_on: Date.new(2022, 1, 1) },
+  { title: "Algorithms", author: "Robert Sedgewick", published_on: Date.new(2011, 3, 19) }
 ])
