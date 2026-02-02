83b71e1f7cb017d1afd5c2802c257ccdec1d52ab
Sam Maharjan <samaharjan1738@gmail.com>
2026-02-02 12:13:41 -0600

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
-  Book.destroy_all
-  { title: "Deep Work", author: "Cal Newport", published_date: Date.new(2016, 1, 5) },
-  { title: "Atomic Habits", author: "James Clear", published_date: Date.new(2018, 10, 16) },
-  { title: "The Phoenix Project", author: "Gene Kim", published_date: Date.new(2013, 1, 10) },
-  { title: "Code Complete", author: "Steve McConnell", published_date: Date.new(2004, 6, 9) },
-  { title: "Working Effectively with Legacy Code", author: "Michael Feathers", published_date: Date.new(2004, 9, 22) }
+Book.destroy_all
+
+Book.create!(title: "Book 1", author: "Author 1", published_date: "2010-01-01")
+Book.create!(title: "Book 2", author: "Author 2", published_date: "2012-05-10")
+Book.create!(title: "Book 3", author: "Author 3", published_date: "2015-09-20")
+Book.create!(title: "Book 4", author: "Author 4", published_date: "2018-03-15")
+Book.create!(title: "Book 5", author: "Author 5", published_date: "2020-11-30")
